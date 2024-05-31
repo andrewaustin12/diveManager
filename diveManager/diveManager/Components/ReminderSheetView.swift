613b055f3a3762d96378e3cd3153435b31c74afb
andrewaustin12 <andrew.d.austin2@gmail.com>
2024-05-31 17:01:13 +0200

    refactored Analytics. monthly income and YTD data

---
+++ diveManager/diveManager/Components/ReminderSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  ReminderSheet.swift
+//  diveManager
+//
+//  Created by andrew austin on 5/30/24.
+//
+
+import SwiftUI
+
+struct ReminderSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReminderSheet()
+}
